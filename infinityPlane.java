f767af1cd6ce0742e2adce2e69a09013f873b491
Matan laptop <matan.harel.mh@gmail.com>
2017-05-16 22:36:10 +0300

    infinityPlane

---
+++ infinityPlane.java
@@ -0,0 +1,20 @@
+
+public class infinityPlane extends AbstractSurface {
+	Vector normal;
+	int offset;
+	
+	public infinityPlane(Material material)
+	{
+		super(material);
+//		this.normal = normal;
+	//	this.offset = offset;
+	}
+	
+
+	@Override
+	public Point intersectes(Vector v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
